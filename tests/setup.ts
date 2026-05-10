bd08a1c996369e9f6a5a60eb6070642fade5deb4
ObsidianThread <179769114+ObsidianThread@users.noreply.github.com>
2026-05-10 21:50:36 +0200

    test: raise asyncUtilTimeout to stabilise picker tests on CI

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,10 +1,13 @@
 import '@testing-library/jest-dom/vitest';
+import { configure } from '@testing-library/dom';
 import emojiData from '../src/entries/data';
 import enLocale from '../src/entries/locales/en';
 import {
   preloadEmojiData,
   registerEmojiLocalePack,
 } from '../src/index';
+
+configure({ asyncUtilTimeout: 5000 });
 
 registerEmojiLocalePack('en', enLocale);
 preloadEmojiData(emojiData);
